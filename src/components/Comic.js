76d572192060635a70274a0c51bfe3516324836d
gabrielfonseca333 <gabrielfonsecag@gmail.com>
2024-10-15 10:34:46 +0200

    10:34 am

--- src/components/Comic.js
+++ src/components/Comic.js
@@ -2,10 +2,21 @@ import { Component } from "react";
 
 class Comic extends Component{
 
+    
+
     render(){
         return(
             <div style={{border:"solid 1px blue"}}>
-                <h2>Soy el hijo comic</h2>
+                <h2>{this.props.comic.titulo}</h2>
+                <p>{this.props.comic.descripcion}</p>
+                <img src={this.props.comic.imagen} style={{width:"250px"}}/>
+                <br></br>
+                <button>
+                    Seleccionar
+                </button>
+                <button>
+                    Eliminar
+                </button>
             </div>
         )
     }
